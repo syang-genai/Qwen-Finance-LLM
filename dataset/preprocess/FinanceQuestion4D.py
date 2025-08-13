4433f1a52c1ed875dea86dc3811b82cb875845bb
syang-genai <s.yang@alumni.duke.edu>
2025-08-12 22:43:04 -0600

    Create FinanceQuestion4D.py

---
+++ dataset/preprocess/FinanceQuestion4D.py
@@ -0,0 +1,19 @@
+from datasets import load_dataset
+
+def dataset_reformat(example):
+    example["prompt"]=[{"role": "user", "content": example["instruction"]}]
+    example["completion"]=[{"role": "assistant", "content": example["output"]}]
+    example["reference_answer"]=example["output"]
+    return example
+    
+
+def FQ4D(split, save_path="../train_dataset/4D/FinanceQuestion"):
+    dataset = load_dataset("4DR1455/finance_questions",split=split)
+    dataset = dataset.map(dataset_reformat, remove_columns=["instruction","output","input"])
+    dataset.save_to_disk(save_path)
+    print(dataset[:1])
+    return dataset 
+
+
+if __name__ == "__main__":
+    FQ4D(split="train", save_path="../train_dataset/4D/FinanceQuestion")
