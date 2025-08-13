c74f31b4c45ccbc0213c7675a02a1167fc7c4559
syang-genai <s.yang@alumni.duke.edu>
2025-08-12 22:43:57 -0600

    Create NYPNaturalReasoningFinance.py

---
+++ dataset/preprocess/NYPNaturalReasoningFinance.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+
+def dataset_reformat(example):
+    example["prompt"]=[{"role": "user", "content": example["question"]}]
+    example["completion"]=[{"role": "assistant", "content": example["responses"][0]["response"]}]
+    return example
+    
+
+def NYRF(save_path="../train_dataset/NeoYiPeng/NaturalReasoningFinance"):
+    dataset = load_dataset("neoyipeng/natural_reasoning_finance",split="train")
+    dataset = dataset.map(dataset_reformat, remove_columns=["question","responses"])
+    dataset.save_to_disk(save_path)
+    print(dataset[:1])
+    return dataset 
+
+
+if __name__ == "__main__":
+    NYRF(save_path="../train_dataset/NeoYiPeng/NaturalReasoningFinance")
