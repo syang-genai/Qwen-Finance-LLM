ca840301c46fe5109860cac26a805e2f9f750443
syang-genai <s.yang@alumni.duke.edu>
2025-08-12 22:40:28 -0600

    Create train_grpo_mixdataset.py

---
+++ dataset/preprocess/train_grpo_mixdataset.py
@@ -0,0 +1,17 @@
+from FinanceQuestion4D import FQ4D
+from NYPNaturalReasoningFinance import NYRF
+from datasets import concatenate_datasets
+
+
+def main():
+    fq4d=FQ4D(split="train", save_path="../train_dataset/4D/FinanceQuestion")
+    nyrf=NYRF(save_path="../train_dataset/NeoYiPeng/NaturalReasoningFinance")
+    
+    dataset=concatenate_datasets([fq4d, nyrf])
+    dataset = dataset.shuffle(seed=42)
+    print(dataset[:1])
+    dataset.save_to_disk("../train_dataset/train_grpo_mixdataset")
+
+
+if __name__ == "__main__":
+    main()
