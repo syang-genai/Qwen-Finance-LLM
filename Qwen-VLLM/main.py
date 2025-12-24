b1c8eab160ab5a9b83ab9fe160b3b82faae8ed39
root <root@ubuntu22.04>
2025-12-24 08:36:09 +0800

    modify mcp

--- Qwen-VLLM/main.py
+++ Qwen-VLLM/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from qwen-vllm!")
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
+def main():
+    model_name = "Qwen/Qwen3-0.6B"
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # Load the model
+    # The model weights will be downloaded to your cache directory (~/.cache/huggingface/hub)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    return 
 
 if __name__ == "__main__":
     main()
